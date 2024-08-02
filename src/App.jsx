c76bfa5e26e675a532061b6a0d0868190956734b
Envi Shy <envicomm.jay@gmail.com>
2024-08-02 16:37:17 +0800

    merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import './App.css'
-import Contact from './components/Contact'
-=======
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 
 import './App.css';
-import Home from './components/Home';
 import Nav from './components/Nav';
 import About from './components/About';
 import Services from './components/Services';
->>>>>>> dev
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,18 +12,12 @@ function App() {
   return (
     <Router>
       <div>
-<<<<<<< HEAD
-   
-          <Contact/>
-
-=======
         <Nav />
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
+
         </Routes>
->>>>>>> dev
       </div>
     </Router>
   );
